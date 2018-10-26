8e3af9effe6e3a5eeac0078e2e301067caaf0597
kylerozzi <42872056+kylerozzi@users.noreply.github.com>
2018-10-26 12:05:24 -0400

    Updated from Brython Server: 10/26/2018 12:05:41 PM

--- cryptography.py
+++ cryptography.py
@@ -1,7 +1,7 @@
 """
 cryptography.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Kyle Rozzi
+Credit: Help from Tristan Meyer
 
 Assignment:
 
@@ -11,9 +11,48 @@ See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptog
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
 
-
-input1=input("Enter e to encrypt, d to decrypt, or q to quit: ")
-if input1=="q":
-    print("Goodbye!")
-        
-            
+def loop():
+    input1=input("Enter e to encrypt, d to decrypt, or q to quit: ")
+    for i in input1:
+        m=[]
+        k=[]
+        if input1=='d':
+            message1=list(input('Message: '))
+            message2=len(message1)
+            key1=list(input('Key: '))
+            while len(key1) <= message2:
+                key1=key1+key1
+            for o in message1:
+                message3=associations.find(o)
+                m.append(message3)
+            for p in key1:
+                key3=associations.find(p)
+                k.append(key3)
+            list1=[(m-k) for m,k in zip(m,k)]
+            for a in list1:
+                e=''.join([associations[s%len(associations)] for s in list1])
+            print(e)
+            loop()
+        elif input1=='e':
+            message1=list(input('Message: '))
+            message2=len(message1)
+            key1=list(input('Key: '))
+            while len(key1) <= message2:
+                key1=key1+key1
+            for o in message1:
+                message3=associations.find(o)
+                m.append(message3)
+            for p in key1:
+                key3=associations.find(p)
+                k.append(key3)
+            list1=[(m+k) for m,k in zip(m,k)]
+            for a in list1:
+                e=''.join([associations[s%len(associations)] for s in list1])
+            print(e)
+            loop()    
+        elif input1=='q':
+            print('Goodbye!')
+        else:
+            print('Did not understand command, try again.')
+            loop()
+loop()
